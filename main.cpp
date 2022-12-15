9f9a07e0d0f3b6b9871517f72eacdeb2ac40970f
wasor4ik <wasor4ik@yandex.ru>
2022-12-15 14:38:12 +0300

    hello world fixed

--- main.cpp
+++ main.cpp
@@ -1,25 +1,9 @@
-#include <QCoreApplication>
-#include <QLocale>
-#include <QTranslator>
 #include <iostream>
 
 using namespace std;
 
-int main(int argc, char *argv[])
+int main()
 {
     cout << "Hello world!" ;
-
-    QCoreApplication a(argc, argv);
-
-    QTranslator translator;
-    const QStringList uiLanguages = QLocale::system().uiLanguages();
-    for (const QString &locale : uiLanguages) {
-        const QString baseName = "Unit1_" + QLocale(locale).name();
-        if (translator.load(":/i18n/" + baseName)) {
-            a.installTranslator(&translator);
-            break;
-        }
-    }
-
-    return a.exec();
+    return 0;
 }
